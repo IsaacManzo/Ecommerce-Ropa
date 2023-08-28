20fd987bbd343bb2f26e4b1335701937fdf20a72
IsaacManzo <isaacemanzo51@gmail.com>
2023-08-28 11:51:04 -0300

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext } from "react";
+// import product context
+import { ProductContext } from "../contexts/ProductContext";
 
 const Home = () => {
-  return <div>Homepage</div>;
+  // get products from product context
+  const { products } = useContext(ProductContext);
+  // get only men´s and women´s clothing category
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+  return (
+    <div>
+      <section className="p-16">
+        <div className="container mx-auto">
+          <div>
+            {filteredProducts.map((product) =>{
+              return <div key={product.id}>{product.title}</div>
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
