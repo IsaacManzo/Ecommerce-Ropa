3ec79c36f3e3d46feb1372da467991547671d9bc
IsaacManzo <isaacemanzo51@gmail.com>
2023-08-28 19:02:48 -0300

    de todo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 // import product context
 import { ProductContext } from "../contexts/ProductContext";
+// import product component
+import Product from "../components/Product"
 
 const Home = () => {
   // get products from product context
@@ -15,9 +17,9 @@ const Home = () => {
     <div>
       <section className="p-16">
         <div className="container mx-auto">
-          <div>
+          <div className="grid gird-col-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
             {filteredProducts.map((product) =>{
-              return <div key={product.id}>{product.title}</div>
+              return <Product product={product} key={product.id}/>
             })}
           </div>
         </div>
