b8fe9dc92ec2f3cff32bbe15304620811959ffc0
IsaacManzo <isaacemanzo51@gmail.com>
2023-08-31 11:50:54 -0300

    Terminado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary py-7">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+            Copyright &copy; Ecommerce Ropa 2023. Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 // import product context
 import { ProductContext } from "../contexts/ProductContext";
-// import product component
+// import component
 import Product from "../components/Product"
+import Hero from "../components/Hero"
 
 const Home = () => {
   // get products from product context
@@ -15,6 +16,7 @@ const Home = () => {
   });
   return (
     <div>
+      <Hero />
       <section className="p-16">
         <div className="container mx-auto">
           <div className="grid gird-col-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
